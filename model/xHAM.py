b02ee11f9219f1854c25f192576c954bbaab926a
Bo Peng <peng10@owens-rw02.ten.osc.edu>
2020-07-02 16:34:56 -0400

    update

--- model/xHAM.py
+++ model/xHAM.py
@@ -4,27 +4,29 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import pdb
 
-class HAM(nn.Module):
+class Model(nn.Module):
     def __init__(self, num_users, num_items, model_args, device):
-        super(HAM, self).__init__()
+        super(Model, self).__init__()
 
         self.args = model_args
+        self.abla = self.args.abla
 
         # init args
         L = self.args.L
+        P = self.args.P
         dims = self.args.d
-
-        #self.V = 16
 
         # user and item embeddings
         self.user_embeddings = nn.Embedding(num_users, dims).to(device)
         self.item_embeddings = nn.Embedding(num_items, dims).to(device)
 
-        #self.WS1 = nn.Linear(dims, self.V)
-        #self.WS2 = nn.Linear(self.V, 3)
+        self.decay = torch.tensor([[1.0]*(L-P) + [2]*P]).view(-1,1).to(device)
 
         self.W2 = nn.Embedding(num_items, dims, padding_idx=0).to(device)
         self.b2 = nn.Embedding(num_items, 1, padding_idx=0).to(device)
+
+        self.cor_gate = nn.Linear(dims, 1)
+        self.mean_gate = nn.Linear(dims,1)
 
         self.user_embeddings.weight.data.normal_(0, 1.0 / self.user_embeddings.embedding_dim)
         self.item_embeddings.weight.data.normal_(0, 1.0 / self.item_embeddings.embedding_dim)
@@ -33,15 +35,25 @@ class HAM(nn.Module):
 
     def forward(self, item_seq, user_ids, items_to_predict, for_pred=False):
         item_embs = self.item_embeddings(item_seq)
-        #low-order is fixed to 2
-        short_item_embs = item_embs[:, -2:]
-
         user_emb = self.user_embeddings(user_ids)
-
         if not for_pred:
             item_embs = F.dropout(item_embs)
 
-        union_out = item_embs.mean(1)
+        short_item_embs = item_embs[:, -self.args.P:]
+        low_order       = short_item_embs.mean(1)
+        high_order      = item_embs.mean(1)
+        union_out   = low_order + high_order
+
+        second_out  = torch.mul(item_embs.unsqueeze(2), item_embs.unsqueeze(1)).sum(2)
+        third_out   = torch.mul(second_out.unsqueeze(2), item_embs.unsqueeze(1)).sum(2)
+        fourt_out   = torch.mul(third_out.unsqueeze(2), item_embs.unsqueeze(1)).sum(2)
+        
+        if self.args.order == 2:
+            union_out = union_out + second_out.mean(1) * high_order
+        elif self.args.order == 3:
+            union_out = union_out + (second_out.mean(1) + third_out.mean(1)) * high_order
+        elif self.args.order == 4:
+            union_out = union_out + (second_out.mean(1) + third_out.mean(1) + fourt_out.mean(1)) * high_order
 
         w2 = self.W2(items_to_predict)
         b2 = self.b2(items_to_predict)
@@ -50,28 +62,15 @@ class HAM(nn.Module):
             w2 = w2.squeeze()
             b2 = b2.squeeze()
 
-            # MF
+            #user-item
             res = user_emb.mm(w2.t()) + b2
-
-            # high-order
+            #item associations
             res += union_out.mm(w2.t())
 
-            # low-order
-            rel_score = torch.matmul(short_item_embs, w2.t().unsqueeze(0))
-            rel_score = torch.mean(rel_score, dim=1)
-            
-            res += rel_score
         else:
-            # MF
+            #user-item
             res = torch.baddbmm(b2, w2, user_emb.unsqueeze(2)).squeeze()
-
-            # high-order
+            #item associations
             res += torch.bmm(union_out.unsqueeze(1), w2.permute(0, 2, 1)).squeeze()
-
-            # low-order
-            rel_score = short_item_embs.bmm(w2.permute(0, 2, 1))
-            rel_score = torch.mean(rel_score, dim=1)
-
-            res += rel_score
 
         return res
